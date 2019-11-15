b16beaef2fca465d931da36b6d8bd5bc2cef7f12
shimehituzi <shimehituzi@gmail.com>
2019-11-15 19:28:01 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-%w(初心者部屋 見習い部屋 中級者部屋 上級者部屋 いい感じの部屋 追加の部屋).each do |room_name|
+%w(room_A room_B room_C room_1 room_2 room_3).each do |room_name|
   Room.create(name: room_name)
   room = Room.find_by(name: room_name)
   10.times do |i|
